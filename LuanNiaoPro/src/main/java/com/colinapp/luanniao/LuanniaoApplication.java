7dd78a1c941dedc76028411f2c57cc95416b1017
colinwps <colinapp@hotmail.com>
2021-06-16 13:03:10 +0800

    init
    初始化

---
+++ LuanNiaoPro/src/main/java/com/colinapp/luanniao/LuanniaoApplication.java
@@ -0,0 +1,13 @@
+package com.colinapp.luanniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuanniaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuanniaoApplication.class, args);
+    }
+
+}
---
+++ LuanNiaoPro/src/test/java/com/colinapp/luanniao/LuanniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colinapp.luanniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuanniaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
