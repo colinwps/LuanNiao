343fe8e61d23b213d879ac7c408d9cf65251c56a
colinwps <colinapp@hotmail.com>
2021-06-28 15:57:09 +0800

    基础文件结构创建

--- LuanNiaoPro/src/test/java/com/colinapp/luanniao/LuanniaoApplicationTests.java
+++ LuanNiaoPro/src/test/java/com/colinapp/luanniao/LuanniaoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.colinapp.luanniao;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class LuanniaoApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        jdbcTemplate.execute("Create Table tt;");
     }
 
 }
